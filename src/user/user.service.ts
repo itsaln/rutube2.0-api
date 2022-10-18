4328aebf9ef2c7e0c07678df14f8f019498268d4
itsaln <itsaln23@gmail.com>
2022-10-18 15:46:47 +0500

    user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common'
+import { Repository } from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm'
+import { UserEntity } from '@app/user/user.entity'
+import { SubscriptionEntity } from '@app/user/subscription.entity'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(
+		@InjectRepository(UserEntity)
+		private readonly userRepository: Repository<UserEntity>,
+		private readonly subscriptionRepository: Repository<SubscriptionEntity>,
+	) {}
+
+	// by id
+
+	// update
+}
